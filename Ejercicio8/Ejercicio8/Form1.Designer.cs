8c87a16eb98a0dd5db63e8584bb7c9826e64f7c2
Ivan-Beltran <ivanbeltran1610@gmail.com>
2024-08-16 09:43:38 -0600

    ejercicio hecho por Miguel Angel

---
+++ Ejercicio8/Ejercicio8/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Ejercicio8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Limpiarbutton = new Button();
+            ResultadotextBox = new TextBox();
+            enviarbutton = new Button();
+            Tittlelabel = new Label();
+            SuspendLayout();
+            // 
+            // Limpiarbutton
+            // 
+            Limpiarbutton.Location = new Point(365, 149);
+            Limpiarbutton.Name = "Limpiarbutton";
+            Limpiarbutton.Size = new Size(75, 23);
+            Limpiarbutton.TabIndex = 5;
+            Limpiarbutton.Text = "limpiar";
+            Limpiarbutton.UseVisualStyleBackColor = true;
+            Limpiarbutton.Click += Limpiarbutton_Click;
+            // 
+            // ResultadotextBox
+            // 
+            ResultadotextBox.Location = new Point(144, 89);
+            ResultadotextBox.Name = "ResultadotextBox";
+            ResultadotextBox.Size = new Size(357, 23);
+            ResultadotextBox.TabIndex = 4;
+            // 
+            // enviarbutton
+            // 
+            enviarbutton.Location = new Point(168, 149);
+            enviarbutton.Name = "enviarbutton";
+            enviarbutton.Size = new Size(75, 23);
+            enviarbutton.TabIndex = 3;
+            enviarbutton.Text = "Enviar";
+            enviarbutton.UseVisualStyleBackColor = true;
+            enviarbutton.Click += enviarbutton_Click;
+            // 
+            // Tittlelabel
+            // 
+            Tittlelabel.AutoSize = true;
+            Tittlelabel.Location = new Point(312, 26);
+            Tittlelabel.Name = "Tittlelabel";
+            Tittlelabel.Size = new Size(57, 15);
+            Tittlelabel.TabIndex = 6;
+            Tittlelabel.Text = "Ejercicio8";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Tittlelabel);
+            Controls.Add(Limpiarbutton);
+            Controls.Add(ResultadotextBox);
+            Controls.Add(enviarbutton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Limpiarbutton;
+        private TextBox ResultadotextBox;
+        private Button enviarbutton;
+        private Label Tittlelabel;
+    }
+}
---
+++ Ejercicio8/Ejercicio8/Form1.cs
@@ -0,0 +1,57 @@
+﻿namespace Ejercicio8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void enviarbutton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(ResultadotextBox.Text, out int numero))
+            {
+                bool divisiblePor3 = numero % 3 == 0;
+                bool divisiblePor11 = numero % 11 == 0;
+
+                if (divisiblePor3 && divisiblePor11)
+                {
+                    MessageBox.Show($"El número {numero} es divisible entre 3 y 11.");
+                }
+                else if (divisiblePor3)
+                {
+                    MessageBox.Show($"El número {numero} es divisible entre 3, pero no entre 11.");
+                }
+                else if (divisiblePor11)
+                {
+                    MessageBox.Show($"El número {numero} es divisible entre 11, pero no entre 3.");
+                }
+                else
+                {
+                    MessageBox.Show($"El número {numero} no es divisible ni entre 3 ni entre 11.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número válido.");
+            }
+        }
+        private void Limpiarbutton_Click(object sender, EventArgs e)
+        {
+            ResultadotextBox.Text = string.Empty;
+
+            ResultadotextBox.Focus();
+        }
+
+        private void ResultadotextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+
+    }
+}
